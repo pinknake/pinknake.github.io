1fe5549278355e62053c035c7c738cd24589d3a2
Ankush Saggu <ankush22004@gmail.com>
2026-02-16 09:29:57 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "kitchen-app-auto";
+
+self.addEventListener("install", event => {
+  self.skipWaiting(); // force activate new version
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
