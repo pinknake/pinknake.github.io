31e751ccaa7e7d927fc7d2fa657a87065cabcc81
Ankush Saggu <ankush22004@gmail.com>
2026-02-22 21:31:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ================= VERSION ================= */
-const CACHE_NAME = "kitchen-manager-v6";
+const CACHE_NAME = "kitchen-manager-v7";
 
 /* ================= APP SHELL ================= */
 const APP_SHELL = [
